cb311defb2e8287d86fc442d0ec0e4d2bb1f843c
wub <wubingzheng@gmail.com>
2026-03-04 21:38:11 +0800

    add u8 and u128

--- src/lib.rs
+++ src/lib.rs
@@ -67,22 +67,96 @@ macro_rules! impl_integer {
     };
 }
 
+impl_integer!(i8, u8);
 impl_integer!(i16, u16);
 impl_integer!(i32, u32);
 impl_integer!(i64, u64);
+impl_integer!(i128, u128);
+
+impl Unsigned for u8 {
+    #[inline]
+    fn dump_len(self) -> usize {
+        let zeros = self.leading_zeros();
+        let bd = unsafe { BITDIGIT_U8.get_unchecked(zeros as usize) };
+        bd.count_digits(self)
+    }
+
+    #[inline]
+    unsafe fn unchecked_dump(self, buf: &mut [u8]) {
+        let mut n = self;
+        let mut offset = buf.len() - 1;
+        loop {
+            let remain = n as usize % 10;
+            unsafe {
+                *buf.get_unchecked_mut(offset) = digit(remain);
+            }
+            offset -= 1;
+            n /= 10;
+            if n == 0 {
+                break;
+            }
+        }
+        debug_assert_eq!(offset, 0);
+    }
+}
+
+impl Unsigned for u128 {
+    #[inline]
+    fn dump_len(self) -> usize {
+        let zeros = self.leading_zeros();
+        let bd = unsafe { BITDIGIT_U128.get_unchecked(zeros as usize) };
+        bd.count_digits(self)
+    }
+
+    unsafe fn unchecked_dump(self, buf: &mut [u8]) {
+        if let Ok(n64) = u64::try_from(self) {
+            return unsafe { u64::unchecked_dump(n64, buf) };
+        }
+
+        // Take the 16 least-significant decimals.
+        let quot_1e16 = self / 10_u128.pow(16);
+        let mod_1e16 = self % 10_u128.pow(16);
+        debug_assert_ne!(quot_1e16, 0);
+
+        let mut remain = mod_1e16 as u64;
+        let mut offset = buf.len();
+
+        // Format per four digits from the lookup table.
+        for _ in 0..4 {
+            offset -= 4;
+
+            // pull two pairs
+            let quad = remain as usize % 1_00_00;
+            remain /= 1_00_00;
+
+            let pair1 = quad / 100;
+            let pair2 = quad % 100;
+            unsafe {
+                *buf.get_unchecked_mut(offset) = digit(pair1 * 2);
+                *buf.get_unchecked_mut(offset + 1) = digit(pair1 * 2 + 1);
+                *buf.get_unchecked_mut(offset + 2) = digit(pair2 * 2);
+                *buf.get_unchecked_mut(offset + 3) = digit(pair2 * 2 + 1);
+            }
+        }
+
+        // Handle quot_1e16
+        unsafe { Self::unchecked_dump(quot_1e16, &mut buf[..offset]) }
+    }
+}
 
 // general implements for u16, u32, and u64
 macro_rules! impl_unsigned {
     ($ty:ty, $bit_digits:expr) => {
         impl Unsigned for $ty {
+            #[inline]
             fn dump_len(self) -> usize {
                 let zeros = self.leading_zeros();
                 let bd = unsafe { $bit_digits.get_unchecked(zeros as usize) };
-                if self >= bd.0 { bd.1 } else { bd.1 - 1 }
+                bd.count_digits(self)
             }
 
             // SAFETY: The caller must make sure: buf.len() == self.dump_len()
-            #[inline(always)]
+            #[inline]
             unsafe fn unchecked_dump(self: $ty, buf: &mut [u8]) {
                 let mut offset = buf.len();
                 let mut remain = self;
@@ -134,6 +208,25 @@ impl_unsigned!(u32, BITDIGIT_U32);
 impl_unsigned!(u16, BITDIGIT_U16);
 
 struct BitDigit<T: PartialOrd>(T, usize);
+
+impl<T: PartialOrd> BitDigit<T> {
+    #[inline]
+    fn count_digits(&self, n: T) -> usize {
+        if n >= self.0 { self.1 } else { self.1 - 1 }
+    }
+}
+
+const BITDIGIT_U8: [BitDigit<u8>; 9] = [
+    BitDigit(0, 3),
+    BitDigit(10_u8.pow(2), 3),
+    BitDigit(0, 2),
+    BitDigit(0, 2),
+    BitDigit(10_u8.pow(1), 2),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+];
 
 const BITDIGIT_U16: [BitDigit<u16>; 17] = [
     BitDigit(0, 5),
@@ -259,8 +352,140 @@ const BITDIGIT_U64: [BitDigit<u64>; 65] = [
     BitDigit(0, 1),
 ];
 
+const BITDIGIT_U128: [BitDigit<u128>; 129] = [
+    BitDigit(0, 39),
+    BitDigit(10_u128.pow(38), 39),
+    BitDigit(0, 38),
+    BitDigit(0, 38),
+    BitDigit(0, 38),
+    BitDigit(10_u128.pow(37), 38),
+    BitDigit(0, 37),
+    BitDigit(0, 37),
+    BitDigit(10_u128.pow(36), 37),
+    BitDigit(0, 36),
+    BitDigit(0, 36),
+    BitDigit(10_u128.pow(35), 36),
+    BitDigit(0, 35),
+    BitDigit(0, 35),
+    BitDigit(0, 35),
+    BitDigit(10_u128.pow(34), 35),
+    BitDigit(0, 34),
+    BitDigit(0, 34),
+    BitDigit(10_u128.pow(33), 34),
+    BitDigit(0, 33),
+    BitDigit(0, 33),
+    BitDigit(10_u128.pow(32), 33),
+    BitDigit(0, 32),
+    BitDigit(0, 32),
+    BitDigit(0, 32),
+    BitDigit(10_u128.pow(31), 32),
+    BitDigit(0, 31),
+    BitDigit(0, 31),
+    BitDigit(10_u128.pow(30), 31),
+    BitDigit(0, 30),
+    BitDigit(0, 30),
+    BitDigit(10_u128.pow(29), 30),
+    BitDigit(0, 29),
+    BitDigit(0, 29),
+    BitDigit(10_u128.pow(28), 29),
+    BitDigit(0, 28),
+    BitDigit(0, 28),
+    BitDigit(0, 28),
+    BitDigit(10_u128.pow(27), 28),
+    BitDigit(0, 27),
+    BitDigit(0, 27),
+    BitDigit(10_u128.pow(26), 27),
+    BitDigit(0, 26),
+    BitDigit(0, 26),
+    BitDigit(10_u128.pow(25), 26),
+    BitDigit(0, 25),
+    BitDigit(0, 25),
+    BitDigit(0, 25),
+    BitDigit(10_u128.pow(24), 25),
+    BitDigit(0, 24),
+    BitDigit(0, 24),
+    BitDigit(10_u128.pow(23), 24),
+    BitDigit(0, 23),
+    BitDigit(0, 23),
+    BitDigit(10_u128.pow(22), 23),
+    BitDigit(0, 22),
+    BitDigit(0, 22),
+    BitDigit(0, 22),
+    BitDigit(10_u128.pow(21), 22),
+    BitDigit(0, 21),
+    BitDigit(0, 21),
+    BitDigit(10_u128.pow(20), 21),
+    BitDigit(0, 20),
+    BitDigit(0, 20),
+    BitDigit(10_u128.pow(19), 20),
+    BitDigit(0, 19),
+    BitDigit(0, 19),
+    BitDigit(0, 19),
+    BitDigit(10_u128.pow(18), 19),
+    BitDigit(0, 18),
+    BitDigit(0, 18),
+    BitDigit(10_u128.pow(17), 18),
+    BitDigit(0, 17),
+    BitDigit(0, 17),
+    BitDigit(10_u128.pow(16), 17),
+    BitDigit(0, 16),
+    BitDigit(0, 16),
+    BitDigit(0, 16),
+    BitDigit(10_u128.pow(15), 16),
+    BitDigit(0, 15),
+    BitDigit(0, 15),
+    BitDigit(10_u128.pow(14), 15),
+    BitDigit(0, 14),
+    BitDigit(0, 14),
+    BitDigit(10_u128.pow(13), 14),
+    BitDigit(0, 13),
+    BitDigit(0, 13),
+    BitDigit(0, 13),
+    BitDigit(10_u128.pow(12), 13),
+    BitDigit(0, 12),
+    BitDigit(0, 12),
+    BitDigit(10_u128.pow(11), 12),
+    BitDigit(0, 11),
+    BitDigit(0, 11),
+    BitDigit(10_u128.pow(10), 11),
+    BitDigit(0, 10),
+    BitDigit(0, 10),
+    BitDigit(0, 10),
+    BitDigit(10_u128.pow(9), 10),
+    BitDigit(0, 9),
+    BitDigit(0, 9),
+    BitDigit(10_u128.pow(8), 9),
+    BitDigit(0, 8),
+    BitDigit(0, 8),
+    BitDigit(10_u128.pow(7), 8),
+    BitDigit(0, 7),
+    BitDigit(0, 7),
+    BitDigit(0, 7),
+    BitDigit(10_u128.pow(6), 7),
+    BitDigit(0, 6),
+    BitDigit(0, 6),
+    BitDigit(10_u128.pow(5), 6),
+    BitDigit(0, 5),
+    BitDigit(0, 5),
+    BitDigit(10_u128.pow(4), 5),
+    BitDigit(0, 4),
+    BitDigit(0, 4),
+    BitDigit(0, 4),
+    BitDigit(10_u128.pow(3), 4),
+    BitDigit(0, 3),
+    BitDigit(0, 3),
+    BitDigit(10_u128.pow(2), 3),
+    BitDigit(0, 2),
+    BitDigit(0, 2),
+    BitDigit(10_u128.pow(1), 2),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+];
+
 // SAFETY: caller must make sure: p < 200
-#[inline(always)]
+#[inline]
 unsafe fn digit(p: usize) -> u8 {
     static DECIMAL_PAIRS: &[u8; 200] = b"\
         0001020304050607080910111213141516171819\
@@ -278,9 +503,9 @@ mod tests {
 
     fn test<T>(n: T)
     where
-        T: Integer + std::fmt::Display + Copy,
+        T: Integer + Copy + std::fmt::Display + std::fmt::Debug,
     {
-        let mut buf: [u8; 20] = [0; 20];
+        let mut buf: [u8; 40] = [0; 40];
         let pos = dump(n, &mut buf).unwrap();
         assert_eq!(str::from_utf8(&buf[..pos]).unwrap(), format!("{n}"));
     }
@@ -291,11 +516,24 @@ mod tests {
         test(i64::MIN);
         test(i64::MAX);
         test(-i64::MAX);
+        test(i128::MIN);
+        test(i128::MAX);
+        test(-i128::MAX);
 
         let mut n = u64::MAX;
         while n != 0 {
             test(n);
             if let Ok(i) = i64::try_from(n) {
+                test(i);
+                test(-i);
+            }
+            n /= 10;
+        }
+
+        let mut n = u128::MAX;
+        while n != 0 {
+            test(n);
+            if let Ok(i) = i128::try_from(n) {
                 test(i);
                 test(-i);
             }
