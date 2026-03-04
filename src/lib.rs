cf2894c00897933be3ec99328a9bc997fa0e2443
wub <wubingzheng@gmail.com>
2026-03-04 13:28:36 +0800

    add trait Integer

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,20 @@
-use core::mem::MaybeUninit;
-
 #[inline(always)]
-pub fn dump(n: u64, buf: &mut [u8]) -> Result<usize, usize> {
-    let len = dump_len(n);
+pub fn dump(n: impl Integer, buf: &mut [u8]) -> Result<usize, usize> {
+    let len = n.dump_len();
     if buf.len() < len {
         return Err(len);
     }
 
     unsafe {
         // SAFETY: `buf[..len]` fits.
-        unchecked_dump(n, &mut buf[..len]);
+        n.unchecked_dump(&mut buf[..len]);
     }
     Ok(len)
 }
 
 #[inline(always)]
-pub fn dump_to_uninit(n: u64, buf: &mut [MaybeUninit<u8>]) -> Result<usize, usize> {
-    // SAFETY: only write the buf, but no read.
-    let buf = unsafe { buf.assume_init_mut() };
-    dump(n, buf)
-}
-
-#[inline(always)]
-pub fn dump_to_string(n: u64, s: &mut String) {
-    let dump_len = dump_len(n);
+pub fn dump_to_string(n: impl Integer, s: &mut String) {
+    let dump_len = n.dump_len();
 
     // SAFETY: all valid charactors.
     // SAFETY: `v[origin_len..]` fits.
@@ -32,70 +23,135 @@ pub fn dump_to_string(n: u64, s: &mut String) {
         let origin_len = v.len();
         v.reserve(dump_len);
         v.set_len(origin_len + dump_len);
-        unchecked_dump(n, &mut v[origin_len..]);
+        n.unchecked_dump(&mut v[origin_len..]);
     }
 }
 
-// SAFETY: The caller must make sure the `buf` just fits the dumped
-// string, no more no less.
-#[inline(always)]
-unsafe fn unchecked_dump(n: u64, buf: &mut [u8]) {
-    let mut offset = buf.len();
-    let mut remain = n;
-
-    // Format per two digits from the lookup table.
-    while remain > 999 {
-        offset -= 4;
-
-        // pull two pairs
-        let quad = remain as usize % 10000;
-        remain /= 10000;
-
-        let pair1 = quad / 100;
-        let pair2 = quad % 100;
-        unsafe {
-            *buf.get_unchecked_mut(offset) = digit(pair1 * 2);
-            *buf.get_unchecked_mut(offset + 1) = digit(pair1 * 2 + 1);
-            *buf.get_unchecked_mut(offset + 2) = digit(pair2 * 2);
-            *buf.get_unchecked_mut(offset + 3) = digit(pair2 * 2 + 1);
-        }
-    }
-    if remain >= 10 {
-        offset -= 2;
-
-        let pair = remain as usize % 100;
-        remain = remain / 100;
-        unsafe {
-            *buf.get_unchecked_mut(offset) = digit(pair * 2);
-            *buf.get_unchecked_mut(offset + 1) = digit(pair * 2 + 1);
-        }
-    }
-
-    // Format the last remaining digit, if any.
-    if remain != 0 || n == 0 {
-        offset -= 1;
-        unsafe {
-            *buf.get_unchecked_mut(offset) = b'0' + remain as u8;
-        }
-    }
-
-    debug_assert_eq!(offset, 0);
+pub trait Integer: Copy {
+    fn dump_len(self) -> usize;
+    unsafe fn unchecked_dump(self, buf: &mut [u8]);
 }
 
-static DECIMAL_PAIRS: &[u8; 200] = b"\
-        0001020304050607080910111213141516171819\
-        2021222324252627282930313233343536373839\
-        4041424344454647484950515253545556575859\
-        6061626364656667686970717273747576777879\
-        8081828384858687888990919293949596979899";
-#[inline(always)]
-fn digit(p: usize) -> u8 {
-    unsafe { *DECIMAL_PAIRS.get_unchecked(p) }
+macro_rules! impl_integer {
+    ($ty:ty, $bit_digits:expr) => {
+        impl Integer for $ty {
+            fn dump_len(self) -> usize {
+                let zeros = self.leading_zeros();
+                let bd = unsafe { $bit_digits.get_unchecked(zeros as usize) };
+                if self >= bd.0 { bd.1 } else { bd.1 - 1 }
+            }
+
+            // SAFETY: The caller must make sure: buf.len() == self.dump_len()
+            #[inline(always)]
+            unsafe fn unchecked_dump(self: $ty, buf: &mut [u8]) {
+                let mut offset = buf.len();
+                let mut remain = self;
+
+                // Format per two digits from the lookup table.
+                while remain >= 1000 {
+                    offset -= 4;
+
+                    // pull two pairs
+                    let quad = remain as usize % 10000;
+                    remain /= 10000;
+
+                    let pair1 = quad / 100;
+                    let pair2 = quad % 100;
+                    unsafe {
+                        *buf.get_unchecked_mut(offset) = digit(pair1 * 2);
+                        *buf.get_unchecked_mut(offset + 1) = digit(pair1 * 2 + 1);
+                        *buf.get_unchecked_mut(offset + 2) = digit(pair2 * 2);
+                        *buf.get_unchecked_mut(offset + 3) = digit(pair2 * 2 + 1);
+                    }
+                }
+                if remain >= 10 {
+                    offset -= 2;
+
+                    let pair = remain as usize % 100;
+                    remain = remain / 100;
+                    unsafe {
+                        *buf.get_unchecked_mut(offset) = digit(pair * 2);
+                        *buf.get_unchecked_mut(offset + 1) = digit(pair * 2 + 1);
+                    }
+                }
+
+                // Format the last remaining digit, if any.
+                if remain != 0 || self == 0 {
+                    offset -= 1;
+                    unsafe {
+                        *buf.get_unchecked_mut(offset) = b'0' + remain as u8;
+                    }
+                }
+
+                debug_assert_eq!(offset, 0);
+            }
+        }
+    };
 }
 
-struct BitDigit(u64, usize);
+impl_integer!(u64, BITDIGIT_U64);
+impl_integer!(u32, BITDIGIT_U32);
+impl_integer!(u16, BITDIGIT_U16);
 
-const BITDIGIT: [BitDigit; 65] = [
+struct BitDigit<T: PartialOrd>(T, usize);
+
+const BITDIGIT_U16: [BitDigit<u16>; 17] = [
+    BitDigit(0, 5),
+    BitDigit(0, 5),
+    BitDigit(10_u16.pow(4), 5),
+    BitDigit(0, 4),
+    BitDigit(0, 4),
+    BitDigit(0, 4),
+    BitDigit(10_u16.pow(3), 4),
+    BitDigit(0, 3),
+    BitDigit(0, 3),
+    BitDigit(10_u16.pow(2), 3),
+    BitDigit(0, 2),
+    BitDigit(0, 2),
+    BitDigit(10_u16.pow(1), 2),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+];
+
+const BITDIGIT_U32: [BitDigit<u32>; 33] = [
+    BitDigit(0, 10),
+    BitDigit(0, 10),
+    BitDigit(10_u32.pow(9), 10),
+    BitDigit(0, 9),
+    BitDigit(0, 9),
+    BitDigit(10_u32.pow(8), 9),
+    BitDigit(0, 8),
+    BitDigit(0, 8),
+    BitDigit(10_u32.pow(7), 8),
+    BitDigit(0, 7),
+    BitDigit(0, 7),
+    BitDigit(0, 7),
+    BitDigit(10_u32.pow(6), 7),
+    BitDigit(0, 6),
+    BitDigit(0, 6),
+    BitDigit(10_u32.pow(5), 6),
+    BitDigit(0, 5),
+    BitDigit(0, 5),
+    BitDigit(10_u32.pow(4), 5),
+    BitDigit(0, 4),
+    BitDigit(0, 4),
+    BitDigit(0, 4),
+    BitDigit(10_u32.pow(3), 4),
+    BitDigit(0, 3),
+    BitDigit(0, 3),
+    BitDigit(10_u32.pow(2), 3),
+    BitDigit(0, 2),
+    BitDigit(0, 2),
+    BitDigit(10_u32.pow(1), 2),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+    BitDigit(0, 1),
+];
+
+const BITDIGIT_U64: [BitDigit<u64>; 65] = [
     BitDigit(10_u64.pow(19), 20),
     BitDigit(0, 19),
     BitDigit(0, 19),
@@ -163,10 +219,17 @@ const BITDIGIT: [BitDigit; 65] = [
     BitDigit(0, 1),
 ];
 
-fn dump_len(n: u64) -> usize {
-    let zeros = n.leading_zeros();
-    let bd = unsafe { BITDIGIT.get_unchecked(zeros as usize) };
-    if n >= bd.0 { bd.1 } else { bd.1 - 1 }
+// SAFETY: caller must make sure: p < 200
+#[inline(always)]
+unsafe fn digit(p: usize) -> u8 {
+    static DECIMAL_PAIRS: &[u8; 200] = b"\
+        0001020304050607080910111213141516171819\
+        2021222324252627282930313233343536373839\
+        4041424344454647484950515253545556575859\
+        6061626364656667686970717273747576777879\
+        8081828384858687888990919293949596979899";
+
+    unsafe { *DECIMAL_PAIRS.get_unchecked(p) }
 }
 
 #[cfg(test)]
